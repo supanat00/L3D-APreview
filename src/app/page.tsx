2bbd95f7d40a2701907789f6c34acd7ad7cd45c4
supanat00 <supanat90@gmail.com>
2024-03-27 00:43:49 +0700

    model-import-test-01

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,9 @@ const Scene = dynamic(() => import('@/components/Scene'), {
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-stretch">      
-    {/* Logo */}
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:blur-2xl before:dark:from-transparent before:lg:h-[360px] z-[-1] p-20">
+    <main className="flex min-h-screen flex-col items-center justify-stretch">
+      {/* Logo */}
+      <div className="flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:blur-2xl before:dark:from-transparent before:lg:h-[360px] z-[-1] p-20">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/tkobro_logo.svg"
@@ -18,11 +18,12 @@ export default function Home() {
           height={500}
           priority
         />
-    
+
       </div>
-    {/* Model Show */}
+      {/* Model Show */}
+      <Scene />
       <div className="text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <Scene />
+
       </div>
     </main>
   );
--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,13 +1,14 @@
-'use client';
-import React from 'react';
-import { Canvas } from '@react-three/fiber';
-import Model from './Model';
+"use client";
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import Model from "./Model";
+import { Environment } from "@react-three/drei";
 
 export default function Scene() {
-    return (
-        <Canvas>
-            <Model />
-        </Canvas>
-    )
-    
+  return (
+    <Canvas>
+      <Environment preset="city" />
+      <Model />
+    </Canvas>
+  );
 }
