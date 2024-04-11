8851beb99d6c6e85a5ffa7a865b71eccc3d1b3fc
supanat00 <supanat90@gmail.com>
2024-04-12 00:52:39 +0700

    alternate-30s-update

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -22,5 +22,81 @@ export const images = [
     {
         id: 6,
         src: "/trainers/3.png",
+    },
+    {
+        id: 7,
+        src: "/trainers/1.png",
+    },
+    {
+        id: 8,
+        src: "/trainers/2.png",
+    },
+    {
+        id: 9,
+        src: "/trainers/3.png",
+    },
+    {
+        id: 10,
+        src: "/trainers/1.png",
+    },
+    {
+        id: 11,
+        src: "/trainers/2.png",
+    },
+    {
+        id: 12,
+        src: "/trainers/3.png",
+    },
+    {
+        id: 13,
+        src: "/trainers/1.png",
+    },
+    {
+        id: 14,
+        src: "/trainers/2.png",
+    },
+    {
+        id: 15,
+        src: "/trainers/3.png",
+    },
+    {
+        id: 16,
+        src: "/trainers/1.png",
+    },
+    {
+        id: 17,
+        src: "/trainers/2.png",
+    },
+    {
+        id: 18,
+        src: "/trainers/3.png",
+    },
+    {
+        id: 19,
+        src: "/trainers/1.png",
+    },
+    {
+        id: 20,
+        src: "/trainers/2.png",
+    },
+    {
+        id: 21,
+        src: "/trainers/3.png",
+    },
+    {
+        id: 22,
+        src: "/trainers/1.png",
+    },
+    {
+        id: 23,
+        src: "/trainers/2.png",
+    },
+    {
+        id: 24,
+        src: "/trainers/3.png",
+    },
+    {
+        id: 25,
+        src: "/trainers/1.png",
     }
 ]
