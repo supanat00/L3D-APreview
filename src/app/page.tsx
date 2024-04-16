881410a73979dbf231eac57a392ee2daf89925d0
supanat00 <supanat90@gmail.com>
2024-04-17 02:24:11 +0700

    update-place-picture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -312,6 +312,17 @@ export default function Home() {
                 sizes="(min-width: 720px) 650px, calc(95.5vw - 19px)"
               />
             ))}
+            {imagesour.map((pic, idx) => (
+              <Image
+                key={idx}
+                className="w-60 mx-3"
+                src={pic.src}
+                alt={""}
+                width={450}
+                height={450}
+                sizes="(min-width: 720px) 650px, calc(95.5vw - 19px)"
+              />
+            ))}
           </div>
         </div>
         <p className="text-gray-400 mt-10 text-center text-md font-Montserrat md:text-2xl lg:text-3xl">M THEATRE & GMM BUILDING FLOOR 22</p>
