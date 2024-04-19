c60f48ec751edfd1aa7c00a8eafa0eba0aa395c3
supanat00 <supanat90@gmail.com>
2024-04-19 15:16:50 +0700

    test-img-slide-03

--- src/components/OurPlaces.tsx
+++ src/components/OurPlaces.tsx
@@ -1,5 +1,7 @@
-
+"use client"
 import { imagesour } from "../utils/constants";
+import { Swiper, SwiperSlide } from "swiper/react"
+
 import Image from 'next/image'
 
 export default function OurPlaces() {
@@ -8,23 +10,25 @@ export default function OurPlaces() {
             {/* OUR PLACE */}
             <p className="text-white text-center font-Montserrat font-bold text-sm md:text-2xl lg:text-3xl">OUR PLACE</p>
             {/* BLKGEM PLACE PICTURE */}
-            <div className="w-full flex flex-col overflow-hidden">
+            <div className="w-full flex flex-row overflow-hidden">
                 {/* line1 */}
-                <div className="ourplaces w-full mt-8 flex">
-                    {imagesour.map((pic, idx) => (
-                        <Image
-                            key={idx}
-                            className="w-60 mx-3"
-                            src={pic.src}
-                            alt={""}
-                            width={450}
-                            height={450}
-                            sizes="(min-width: 720px) 650px, calc(95.5vw - 19px)"
-                        />
-                    ))}
-                </div>
-            </div>
+                <Swiper className="mt-8 flex w-48">
 
+                    {imagesour.map((pic, idx) => (
+                        <SwiperSlide className="mx-4" key={idx}>
+                            <Image
+                                className="w-full"
+                                src={pic.src}
+                                alt={""}
+                                width={450}
+                                height={450}
+                                sizes="(min-width: 720px) 650px, calc(95.5vw - 19px)"
+                            />
+                        </SwiperSlide>
+                    ))}
+
+                </Swiper>
+            </div>
             <p className="text-gray-400 mt-10 text-center text-md font-Montserrat md:text-2xl lg:text-3xl">M THEATRE & GMM BUILDING FLOOR 22</p>
         </div>
 
